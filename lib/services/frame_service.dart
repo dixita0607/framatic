4dd91ad5bcc628707560097defc2046b432c64c6
Dixita <contact@dixita.dev>
2026-02-10 12:33:48 +0530

    refactor: implement frame service internally using sqflite
     - Implement CRUD for frames
     - Handle errors for the relevant database operations

--- lib/services/frame_service.dart
+++ lib/services/frame_service.dart
@@ -10,8 +10,47 @@ class FrameService {
     return frames.map((frame) => Frame.fromJson(frame)).toList();
   }
 
-  // getFrameById
-  // createFrame
-  // updateFrame
-  // deleteFrame
+  Future<Frame> getFrameById(int id) async {
+    final frame = await db.query(
+      FramesTable.name,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+    if (frame.isEmpty) {
+      throw StateError('Frame not found with given id: $id');
+    }
+    return Frame.fromJson(frame[0]);
+  }
+
+  Future<Frame> createFrame(Frame frame) async {
+    final createdFrameId = await db.insert(FramesTable.name, frame.toJson());
+    if (createdFrameId == 0) throw StateError('Failed to create the frame');
+    return await getFrameById(createdFrameId);
+  }
+
+  Future<Frame> updateFrame(Frame frame) async {
+    if (frame.id == null) {
+      throw ArgumentError('frame.id cannot be null for update operation');
+    }
+    final updatedFrameId = await db.update(
+      FramesTable.name,
+      frame.toJson(),
+      where: 'id = ?',
+      whereArgs: [frame.id],
+    );
+    if (updatedFrameId == 0) {
+      throw StateError('Failed to update the frame with id: ${frame.id}');
+    }
+    return await getFrameById(frame.id!);
+  }
+
+  Future<int> deleteFrame(int id) async {
+    final deletedFrame = await db.delete(
+      FramesTable.name,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+    if (deletedFrame == 1) return id;
+    throw StateError('Failed to delete the frame with given id: $id');
+  }
 }
